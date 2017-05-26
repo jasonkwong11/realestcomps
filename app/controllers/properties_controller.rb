88407a98f05179abcfead775f0d90abb57e96ee2
Jason Kwong <jasonkwong11@gmail.com>
2017-05-26 16:47:07 -0400

    add more resilency on principle property object

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -33,13 +33,13 @@ class PropertiesController < ApplicationController
       comps = Rillow.new('X1-ZWz1fgbe5szxmz_29gz1')
       response = comps.get_deep_comps(principle_property_zpid, 25)
 
-      @principle_property.percentile = response["response"][0]["properties"][0]["principal"][0]["zestimate"][0]["percentile"][0]
-      @principle_property.year_built = response["response"][0]["properties"][0]["principal"][0]["yearBuilt"][0]
-      @principle_property.square_feet = response["response"][0]["properties"][0]["principal"][0]["lotSizeSqFt"][0]
-      @principle_property.bathrooms = response["response"][0]["properties"][0]["principal"][0]["bathrooms"][0]
-      @principle_property.bedrooms = response["response"][0]["properties"][0]["principal"][0]["bedrooms"][0]
-      @principle_property.last_sold_date = response["response"][0]["properties"][0]["principal"][0]["lastSoldDate"][0]
-      @principle_property.last_sold_price = response["response"][0]["properties"][0]["principal"][0]["lastSoldPrice"][0]["content"]
+      @principle_property.percentile = response["response"][0]["properties"][0]["principal"][0]["zestimate"][0]["percentile"] ? response["response"][0]["properties"][0]["principal"][0]["zestimate"][0]["percentile"][0] : 'N/A'
+      @principle_property.year_built = response["response"][0]["properties"][0]["principal"][0]["yearBuilt"] ? response["response"][0]["properties"][0]["principal"][0]["yearBuilt"][0] : 'N/A'
+      @principle_property.square_feet = response["response"][0]["properties"][0]["principal"][0]["lotSizeSqFt"] ? response["response"][0]["properties"][0]["principal"][0]["lotSizeSqFt"][0] : 'N/A'
+      @principle_property.bathrooms = response["response"][0]["properties"][0]["principal"][0]["bathrooms"] ? response["response"][0]["properties"][0]["principal"][0]["bathrooms"][0] : 'N/A'
+      @principle_property.bedrooms = response["response"][0]["properties"][0]["principal"][0]["bedrooms"] ? response["response"][0]["properties"][0]["principal"][0]["bedrooms"][0] : 'N/A'
+      @principle_property.last_sold_date = response["response"][0]["properties"][0]["principal"][0]["lastSoldDate"] ? response["response"][0]["properties"][0]["principal"][0]["lastSoldDate"][0] : 'N/A'
+      @principle_property.last_sold_price = response["response"][0]["properties"][0]["principal"][0]["lastSoldPrice"] ? response["response"][0]["properties"][0]["principal"][0]["lastSoldPrice"][0]["content"] : 'N/A'
 
       #@principle_property object is has all of it's associated data
       #now iterate through the comparables has to instantiate new comp objects
